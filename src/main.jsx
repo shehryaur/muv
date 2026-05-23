b3740e1727125bb8475b870d561738fa1e959554
Shehryar Hassan <malik.shehryar.hassan05@gmail.com>
2026-05-23 20:23:55 +0500

    Initial imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
+import WebApp from '@twa-dev/sdk';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+WebApp.ready();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
